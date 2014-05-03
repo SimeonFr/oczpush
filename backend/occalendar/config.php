a3655a68a78f7068f36d2dd905d6d3195689c5ab
Simeon <Simeon@Simeons-PC>
2014-05-03 14:15:00 +0200

    changed .gitignore to not ignore all config.php's

---
+++ backend/occalendar/config.php
@@ -0,0 +1,16 @@
+<?php
+/***********************************************
+* File      :   backend/occalendar/config.php
+* Project   :   Z-Push
+* Descr     :   OCCalendar backend configuration file
+
+* License   : 	AGPL (like original)
+************************************************/
+
+// ************************
+//  BackendOCCalendar settings
+// ************************
+
+define('OC_DIR', OC_DIR);
+
+?>
---
+++ backend/occontacts/config.php
@@ -0,0 +1,16 @@
+<?php
+/***********************************************
+* File      :   backend/occontacts/config.php
+* Project   :   Z-Push
+* Descr     :   OCContacts backend configuration file
+
+* License   : 	AGPL (like original)
+************************************************/
+
+// ************************
+//  BackendOCContacts settings
+// ************************
+
+define('OC_DIR', OC_DIR);
+
+?>
